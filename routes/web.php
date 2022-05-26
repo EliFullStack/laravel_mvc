e3643906180f7bf5bd08a27d84741735e09d1589
unknown <elisabet.v.alvarez@gmail.com>
2022-05-26 10:36:52 +0200

    assigning routes to EntidadController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EquipoController;
 use App\Http\Controllers\PartidoController;
+use App\Http\Controllers\EntidadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,33 +34,10 @@ Route::get('partidos/create', [PartidoController::class, 'create']);
 
 Route::get('partidos/{partido}', [PartidoController::class, 'show']);
 
-Route::get('planteles', function () {
-    return "PLANTELES DE JUGADORAS";
-});
 
-Route::get('planteles/{plantel}/{player?}', function ($plantel,$player=null) {
-    if($player) {
-        return "Aquí encuentras el profile de la jugadora: $player";
-    } else {
-        return "PLANTEL: $plantel";
-    }
-    
-});
 
-Route::get('entidades', function () {
-    return "ESTAS SON LAS ENTIDADES";
-});
+Route::get('entidades', [EntidadController::class, 'index']);
 
-Route::get('entidades/{entidad}/{equipo?}/{player?}', function($entidad, $equipo = null, $player = null) {
-    if($player) {
-        return "Esta es la jugadora $player, del equipo $equipo, que pertenece a $entidad";
-    } elseif ($equipo) {
-        return "Este es el equipo $equipo, que pertenece a $entidad";
-    } else {
-        return "ENTIDAD: $entidad";
-    };
-
-    
-});
+Route::get('entidades/{entidad}/{equipo?}/{player?}', [EntidadController::class, 'show']);
 
 
