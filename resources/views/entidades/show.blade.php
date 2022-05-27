acf357246793231f6c9eaf6466481466ead64d38
unknown <elisabet.v.alvarez@gmail.com>
2022-05-27 15:37:44 +0200

    modifying show.blade.php

--- resources/views/entidades/show.blade.php
+++ resources/views/entidades/show.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.plantilla')
 @section('title', 'Entidades show')
 @section('content')
-<p>
+
     <?php 
-    if($player) {
-            echo "Esta es la jugadora $player, del equipo $equipo, que pertenece a $entidad";
-        } elseif ($equipo) {
-            echo "Este es el equipo $equipo, que pertenece a $entidad";
-        } else {
-            echo "ENTIDAD: $entidad";
-        };
-    ?>
-    </p>
+    if($player) {?>
+            <h1>"Esta es la jugadora {{$player}}, del equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1> ;
+        <?php } elseif ($equipo) {?>
+            <h1>"Este es el equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1>;
+       <?php } else {?>
+            <h1>"ENTIDAD: {{$entidad}}"</h1>;
+     <?php   };?>
+
+    
 @endsection
 
 
