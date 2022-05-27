ac3d27c25b0ddb336020585f4070d2a23a8929d0
unknown <elisabet.v.alvarez@gmail.com>
2022-05-27 18:55:38 +0200

    using blade directives in show.blade.php

--- resources/views/entidades/show.blade.php
+++ resources/views/entidades/show.blade.php
@@ -2,14 +2,14 @@
 @section('title', 'Entidades show')
 @section('content')
 
-    <?php 
-    if($player) {?>
-            <h1>"Esta es la jugadora {{$player}}, del equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1> ;
-        <?php } elseif ($equipo) {?>
-            <h1>"Este es el equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1>;
-       <?php } else {?>
-            <h1>"ENTIDAD: {{$entidad}}"</h1>;
-     <?php   };?>
+    
+    @if($player)
+        <h1>"Esta es la jugadora {{$player}}, del equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1>
+    @elseif ($equipo)
+        <h1>"Este es el equipo {{$equipo}}, que pertenece a {{$entidad}}"</h1>
+    @else
+        <h1>"ENTIDAD: {{$entidad}}"</h1>
+    @endif
 
     
 @endsection
