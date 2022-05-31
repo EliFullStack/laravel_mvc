b1aa2c8180292cff04c76c2ac7f7111aecd2aa85
unknown <elisabet.v.alvarez@gmail.com>
2022-05-31 23:28:29 +0200

    creating EntidadFactory.php

---
+++ database/factories/EntidadFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Entidad;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EntidadFactory extends Factory
+{
+    protected $model = Entidad::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'nombre' => $this->faker->sentence(),
+            'pais_origen'=>$this->faker->country()
+        ];
+    }
+}
