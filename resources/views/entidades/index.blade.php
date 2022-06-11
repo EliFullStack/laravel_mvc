54cddbef851f48b546c3f229b0c8d1517ef7d989
unknown <elisabet.v.alvarez@gmail.com>
2022-06-11 19:44:47 +0200

    changes in entidades.index

--- resources/views/entidades/index.blade.php
+++ resources/views/entidades/index.blade.php
@@ -6,32 +6,32 @@
 
 <div class="container bg-violet-700">
 <section class="entidades">
-    <h2 class="text-4xl font-bold text-white pt-5 mb-5 text-center">Entidades</h2>
+    <h2 class="text-4xl font-semibold text-white pt-5 mb-5 text-center">Entidades</h2>
 
     <div class="flex justify-around">
         <div>
             <a href="#">
                 <div class="a bg-cover mx-auto  border-2 border-white-500/50 rounded"></div>
             </a>            
-            <h3 class="font-mono text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "A"</h3>            
+            <h3 class="h2 text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "A"</h3>            
         </div>
         <div>
             <a href="#">
             <div class="b bg-cover mx-auto border-2 border-white-500/50 rounded"></div>
             </a>
-            <h3 class="font-mono text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "B"</h3>            
+            <h3 class="h2 text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "B"</h3>            
         </div>
         <div>
             <a href="#">
             <div class="c bg-cover mx-auto border-2 border-white-500/50 rounded"></div>
             </a>
-            <h3 class="font-mono text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "C"</h3>            
+            <h3 class="h2 text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "C"</h3>            
         </div>
         <div>
             <a href="#">
             <div class="d bg-cover mx-auto border-2 border-white-500/50 rounded"></div>
             </a>
-            <h3 class="font-mono text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "D"</h3>            
+            <h3 class="h2 text-2xl font-medium text-white pt-5 text-center pb-5">Entidad "D"</h3>            
         </div>
     </div>               
             <!--    <ul>
